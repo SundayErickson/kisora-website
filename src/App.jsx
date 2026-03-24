811ce23d251ad10158ea8af5783ee43e785dfb54
SundayErickson <133476358+SundayErickson@users.noreply.github.com>
2026-03-24 14:37:20 +0300

    Create src/App.jsx with KISORA website homepage component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to the KISORA Website</h1>
+            <p>Your go-to place for all things KISORA!</p>
+        </div>
+    );
+};
+
+export default HomePage;
