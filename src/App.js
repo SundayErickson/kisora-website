e8cd8a7d6fcf105f8c59a63472dffc2e5a0c0ff2
SundayErickson <133476358+SundayErickson@users.noreply.github.com>
2026-03-24 17:31:21 +0300

    Create src/App.js with KisoraWebsite React component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const KisoraWebsite = () => {
+    return (
+        <div>
+            <h1>Welcome to Kisora Website!</h1>
+            <p>This is a simple React component.</p>
+        </div>
+    );
+};
+
+export default KisoraWebsite;
